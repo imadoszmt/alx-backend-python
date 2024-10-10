288fdd004e4791bdea7b194e9294a2bb88ad784e
Imad Zalmat <imaden.zalmat@gmail.com>
2024-10-10 19:12:05 +0000

    Add type-annotated function that takes a float number and return its string version

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/ust/bin/env python3
+"""
+A module that convert a float number to its string version.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    A type-annotated function that take a float number and return
+    its string version.
+
+    Args:
+        n(float): Float number parameter.
+    """
+    return str(n)
