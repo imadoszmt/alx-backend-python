52ebc35d4d39976aa6afbe5b49ce2e3d44e4ddb3
Imad Zalmat <imaden.zalmat@gmail.com>
2024-10-10 18:53:57 +0000

    Add a type-annoted function that return the floor version of a float number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A module that convert float number to floor.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    A function that take a float as an argument and return its floor version.
+
+    Args:
+        n (float): Float number argument.
+    """
+    return n//1
