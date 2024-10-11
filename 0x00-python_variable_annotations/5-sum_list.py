747923ba9b2bdb5c5cf13b870532d598b1b2391f
Imad Zalmat <imaden.zalmat@gmail.com>
2024-10-11 01:53:01 +0000

    Add a type-annotated summing function that takes a list of floats as a parameter and return their sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+A module that sum a lsi of float numbers.
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    A function that take a list of float as a parameter and
+    return their sum.
+
+    Args:
+        input_list(list): A list of float as a parameter.
+    """
+    return sum(input_list)
